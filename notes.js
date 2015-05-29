69fccd552c66f72da98454bb435a53796257f164
Manuel Vila <mvila@3base.com>
2015-05-29 13:34:47 +0800

    switch to ES6

--- notes.js
+++
@@ -1,8 +0,0 @@
-var db = WebSQL.create('AVCMobileTest');
-
-var res = yield db.query('SELECT 1 + 1');
-
-yield db.transaction(function(tr) {
-  var res1 = yield tr.query('...');
-  var res2 = yield tr.query('...');
-});
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 'use strict';
 
-var _ = require('lodash');
-var co = require('co');
-var KindaObject = require('kinda-object');
+let _ = require('lodash');
+let co = require('co');
+let KindaObject = require('kinda-object');
 
-var KindaWebSQL = KindaObject.extend('KindaWebSQL', function() {
-  this.setCreator(function(name, options) {
-    if (!options) options = {};
-    var shortName = name;
-    var version = options.version || '1.0';
-    var displayName = options.displayName || name;
-    var maxSize = options.maxSize || (50 * 1024 * 1024); // 50 MB
+let KindaWebSQL = KindaObject.extend('KindaWebSQL', function() {
+  this.creator = function(options = {}) {
+    if (!options.name) throw new Error('WebSQL database name is missing');
+    let shortName = options.name;
+    let version = options.version || '1.0';
+    let displayName = options.displayName || options.name;
+    let maxSize = options.maxSize || (50 * 1024 * 1024); // 50 MB
     this.database = openDatabase(shortName, version, displayName, maxSize);
-  });
+  };
 
   this.query = function(sql, values) {
-    var that = this;
+    let that = this;
     values = that.normalizeValues(values);
-    var result = undefined;
+    let result;
     return function(cb) {
       that.database.transaction(function(tr) {
-        tr.executeSql(sql, values, function(tr, res) {
+        tr.executeSql(sql, values, function(innerTr, res) {
           result = that.normalizeResult(res);
         });
       }, function(err) { // transaction error callback
@@ -31,24 +31,24 @@ var KindaWebSQL = KindaObject.extend('KindaWebSQL', function() {
     };
   };
 
-  this.transaction = function(fn, options) {
-    var that = this;
+  this.transaction = function(fn) {
+    let that = this;
     return function(cb) {
-      var lastErr;
-      var transactionAborted;
+      let lastErr;
+      let transactionAborted;
       that.database.transaction(function(tr) {
         co(function *() {
           try {
             yield fn({
-              query: function(sql, values) {
+              query(sql, values) {
                 values = that.normalizeValues(values);
-                return function(innerCB) {
+                return function(innerCb) {
                   try {
-                    tr.executeSql(sql, values, function(tr, res) {
-                      innerCB(null, that.normalizeResult(res));
-                    }, function(tr, err) {
+                    tr.executeSql(sql, values, function(innerTr, res) {
+                      innerCb(null, that.normalizeResult(res));
+                    }, function(innerTr, err) {
                       transactionAborted = true;
-                      innerCB(err);
+                      innerCb(err);
                       return true;
                     });
                   } catch (err) {
@@ -76,10 +76,11 @@ var KindaWebSQL = KindaObject.extend('KindaWebSQL', function() {
   this.normalizeValues = function(values) {
     if (values && values.length) {
       values = _.map(values, function(val) {
-        if (typeof val === 'undefined')
-          val = null
-        else if (Buffer.isBuffer(val))
-          val = "bin!" + val.toString('hex');
+        if (typeof val === 'undefined') {
+          val = null;
+        } else if (Buffer.isBuffer(val)) {
+          val = 'bin!' + val.toString('hex');
+        }
         return val;
       });
     }
@@ -88,20 +89,25 @@ var KindaWebSQL = KindaObject.extend('KindaWebSQL', function() {
 
   this.normalizeResult = function(result) {
     if (!result) return result;
-    var normalizedResult = [];
-    if (result.rowsAffected != null)
+    let normalizedResult = [];
+    if (result.rowsAffected != null) {
       normalizedResult.affectedRows = result.rowsAffected;
+    }
     try {
-      if (result.insertId != null)
+      if (result.insertId != null) {
         normalizedResult.insertId = result.insertId;
-    } catch (err) {}
+      }
+    } catch (err) {
+      // noop
+    }
     if (!result.rows) return normalizedResult;
-    for (var i = 0; i < result.rows.length; i++) {
-      var row = result.rows.item(i);
-      var normalizedRow = {};
-      _.forOwn(row, function(val, key) {
-        if (val && val.substr && val.substr(0, 4) === 'bin!')
+    for (let i = 0; i < result.rows.length; i++) {
+      let row = result.rows.item(i);
+      let normalizedRow = {};
+      _.forOwn(row, function(val, key) { // eslint-disable-line no-loop-func
+        if (val && val.substr && val.substr(0, 4) === 'bin!') {
           val = new Buffer(val.substr(4), 'hex');
+        }
         normalizedRow[key] = val;
       });
       normalizedResult.push(normalizedRow);
